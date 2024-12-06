815fceef1de175d3af147b6eac288cb44424e50c
zhaoxiangyu <liseami@qq.com>
2024-12-06 22:34:28 +0800

    fix files

--- AlipayDemo/MVVM/Views/InputView.swift
+++ AlipayDemo/MVVM/Views/InputView.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 
 struct InputView: View {
-    
-    @Binding var  amountIntput : String
-    @Binding var  nameInput : String
-    @Binding var  isOpenSheet : Bool
-    @Binding var date : Date
+    @Binding var amountIntput: String
+    @Binding var nameInput: String
+    @Binding var isOpenSheet: Bool
+    @Binding var date: Date
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
             Text("请输入你想要的金额")
@@ -25,7 +24,7 @@ struct InputView: View {
                 .multilineTextAlignment(.center)
                 .font(.title2)
                 .textFieldStyle(.roundedBorder)
-            
+
             DatePicker("日期选择", selection: $date, displayedComponents: .date)
             DatePicker("日期选择", selection: $date, displayedComponents: .hourAndMinute)
                 .datePickerStyle(.wheel)
@@ -37,15 +36,11 @@ struct InputView: View {
                 Text("确认")
             }
             .buttonStyle(.bordered)
-
         }
         .padding()
-    
-        
-        
     }
 }
 
 #Preview {
-    InputView(amountIntput: .constant("1000.00"), nameInput: .constant("金巧巧"), isOpenSheet: .constant(true),date: .constant(.now))
+    InputView(amountIntput: .constant("1000.00"), nameInput: .constant("金巧巧"), isOpenSheet: .constant(true), date: .constant(.now))
 }
